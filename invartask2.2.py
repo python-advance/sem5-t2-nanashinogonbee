1bb3395d419b039b5f9bfe01a01489866f058ba5
nanashinogonbee <31703502+nanashinogonbee@users.noreply.github.com>
2020-06-28 14:21:14 +0300

    Swap 2.1 and 2.2

--- invartask2.2.py
+++ invartask2.2.py
@@ -1,8 +1,22 @@
-def fibonacci(n):
-    fib1, fib2 = 0, 1
-    for _ in range(n):
-        fib1, fib2 = fib2, fib1 + fib2
-        yield fib1
+class Fibonacci:
+    def __init__(self):
+        self.result = [0, 1]
+
+    def __iter__(self):
+        self.initial = self.result
+        return self
+
+    def __next__(self):
+        self.initial.append(self.initial[-1] + self.initial[-2])
+
+    def find(self, max):
+        while max >= self.result[-1]:
+            self.result.append(self.result[-1] + self.result[-2])
+        if max < self.result[-1]:
+            self.result.pop()
+        return self.result
 
 
-print(list(fibonacci(5)))
+fib = Fibonacci()
+print(fib.find(int(''.join(x for x in input(': ') if x.isdigit()))))
+
